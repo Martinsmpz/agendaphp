5c4d5e66bebcbe795ef3e17acc1a1adfb7e02ced
Gabriel Martins <kacimaang25@gmail.com>
2023-08-09 21:57:45 -0300

    commit agenda

---
+++ create.php
@@ -0,0 +1,31 @@
+ <?php 
+ include_once("templates/header.php");
+ 
+ ?>
+ <div class="container">
+
+     <?php include_once("templates/backbtn.html") ?>
+     <h1 id="main-title">Create Contact</h1>
+     <form id="create-form" action="<?= $BASE_URL?>config/process.php" method="POST">
+         <input type="hidden" name="type" value="create">
+         <div class="form-group">
+             <label for="name">Contact name</label>
+             <input type="text" class="form-control" id="name" name="name" placeholder="Put contact name" required>
+         </div>
+         <div class="form-group">
+             <label for="phone">Contact phone:</label>
+             <input type="text" class="form-control" id="phone" name="phone" placeholder="Put contact phone number"
+                 required>
+         </div>
+         <div class="form-group">
+             <label for="observations">Observations:</label>
+             <textarea type="text" class="form-control" id="observations" name="observations"
+                 placeholder="Put contact observations" rows="3"> </textarea>
+
+             <button type="submit" class=" btn btn-primary">Register</button>
+     </form>
+ </div>
+
+ <?php 
+ include_once("templates/footer.php");
+ ?>
